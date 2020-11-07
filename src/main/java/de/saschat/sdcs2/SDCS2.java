fd66ef4ed5cda87d2ff5a413fa3194bd85e672b9
Sascha_T <info@sascha-t.de>
2020-11-08 00:27:48 +0100

    Initial commit. No code

---
+++ src/main/java/de/saschat/sdcs2/SDCS2.java
@@ -0,0 +1,13 @@
+package de.saschat.sdcs2;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+
+public class SDCS2 implements DedicatedServerModInitializer {
+
+    @Override
+    public void onInitializeServer() {
+
+    }
+}
